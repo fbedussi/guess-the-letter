4cd0948e977635878aabd14467bac513ebbbc945
Francesco Bedussi <francesco.bedussi@abstract.it>
2023-08-25 15:23:09 +0200

    stats

--- script.js
+++ script.js
@@ -14,9 +14,21 @@ const ui = {
   pause: document.querySelector('.pause-btn'),
   mute: document.querySelector('.mute-btn'),
   level: document.querySelector('.level span'),
+  statsBtn: document.querySelector('.stats-btn'),
+  stats: {
+    modal: document.querySelector('dialog'),
+    rounds: document.querySelector('#rounds'),
+    right: document.querySelector('#right'),
+    wrong: document.querySelector('#wrong'),
+    avgTime: document.querySelector('#avg-time'),
+    maxTime: document.querySelector('#max-time'),
+    minTime: document.querySelector('#min-time'),
+    graph: document.querySelector('#graph'),
+  }
 }
 
 const MAX_SCORE = 'max-score'
+const NUMBER_OF_LETTERS = 26
 
 const state = {
   score: 0,
@@ -29,6 +41,11 @@ const state = {
   rounds: 0,
   level: 1,
   maxTime: 5,
+}
+
+const stats = {
+  ok: [],
+  ko: [],
 }
 
 const startInterval = () => {
@@ -62,7 +79,7 @@ const startTime = () => {
 }
 
 const startRound = () => {
-  const letterCode = Math.round(Math.random()*24) + 65
+  const letterCode = Math.round(Math.random()*NUMBER_OF_LETTERS) + 65
   state.char = String.fromCharCode(letterCode)
   ui.char.textContent = state.char
   
@@ -104,7 +121,14 @@ const setMaxScore = (newMaxScore) => {
 }
 
 const updatePoints = (guessed) => {
+  const {char, time, level, intervalDuration} = state
   if (guessed) {
+    stats.ok.push({
+      char, 
+      time, 
+      level, 
+      intervalDuration,
+    })
     ui.playScreen.classList.add('animate-success')
     
     setScore(state.score + state.time)
@@ -114,6 +138,12 @@ const updatePoints = (guessed) => {
     }
     return playWonTune()
   } else {
+    stats.ko.push({
+      char, 
+      time, 
+      level, 
+      intervalDuration,
+    })
     ui.playScreen.classList.add('animate-error')
     
     setScore(state.score - (state.maxTime - state.time))
@@ -151,7 +181,7 @@ ui.clearScore.addEventListener('click', () => {
   setScore(0)
 })
 
-ui.pause.addEventListener('click', () => {
+const pause = () => {
   if (state.interval) {
     clearInterval(state.interval)
     state.interval = undefined
@@ -162,7 +192,9 @@ ui.pause.addEventListener('click', () => {
     ui.char.classList.remove('paused')
     ui.pause.classList.remove('active')
   }
-})
+}
+
+ui.pause.addEventListener('click', pause)
 
 ui.mute.addEventListener('click', () => {
   if (state.muted) {
@@ -174,4 +206,69 @@ ui.mute.addEventListener('click', () => {
     state.muted = true
     ui.mute.classList.add('active')
   }
+})
+
+const calculateStats = () => {
+  const rounds = stats.ok.length + stats.ko.length
+  const right = stats.ok.length
+  const wrong = stats.ko.length
+  const okAndKo = stats.ok.concat(stats.ko)
+  const times = okAndKo.map((item) => (state.maxTime - item.time) * item.intervalDuration)
+  const totalTime = times.reduce((tot, time) => tot + time, 0) 
+  const avgTime = totalTime > 0 ? Math.round(totalTime / rounds) : 0
+  const maxTime = times.length ? Math.max(...times) : '-'
+  const minTime = times.length ? Math.min(...times) : '-'
+  const chars = new Array(NUMBER_OF_LETTERS).fill(undefined).map((_, index) => String.fromCharCode(65 + index))
+  const charsStats = chars.map(char => ({
+    char,
+    shown: okAndKo.filter(item => item.char === char).length,
+    guessed: stats.ok.filter(item => item.char === char).length,
+    notGuessed: stats.ko.filter(item => item.char === char).length,
+  })).map(stats => ({
+    ...stats, 
+    guessRate: stats.shown > 0 ? stats.guessed / stats.shown * 100 : -1,
+  }))//.sort((a, b) => b.guessRate - a.guessRate)
+
+  ui.stats.rounds.innerText = rounds
+  ui.stats.right.innerText = right
+  ui.stats.wrong.innerText = wrong
+  ui.stats.avgTime.innerText = avgTime
+  ui.stats.maxTime.innerText = maxTime
+  ui.stats.minTime.innerText = minTime
+
+  const charsStatsToShow = charsStats//.filter(({guessRate}) => guessRate > -1)
+  
+  ui.stats.graph.innerHTML = charsStatsToShow.map(({char}) => `
+    <div class="char-stat">
+      <div class="char-stat_bar">
+        <div class="char-stat_bar_ok"></div>
+        <div class="char-stat_bar_ko"></div>
+      </div>
+      <div class="char-stat_label">${char}</div>
+    </div>
+  `).join('')
+
+   window.requestAnimationFrame(() => {
+    Array.from(document.querySelectorAll('.char-stat_bar')).forEach((bar, index) => {
+      const okBar = bar.querySelector('.char-stat_bar_ok')
+      const koBar = bar.querySelector('.char-stat_bar_ko')
+      const {guessRate} = charsStatsToShow[index]
+      const okPerc = guessRate > -1 ? guessRate : 0
+      const koPerc = guessRate > -1 ? 100 - guessRate : 0
+      okBar.style.flex = `${okPerc}%`
+      koBar.style.flex = `${koPerc}%`
+      if (okPerc === 0) {
+        okBar.style.display = 'none'
+      }
+      if (koPerc === 0) {
+        koBar.style.display = 'none'
+      }
+    })
+  })
+}
+
+ui.statsBtn.addEventListener('click', () => {
+  pause()
+  calculateStats()
+  ui.stats.modal.showModal()
 })
